247561cb911a68808d04cd2145baff2bbe60942b
Paul Bridger <paul@paulbridger.com>
2020-08-05 12:37:33 +0200

    simple pipeline

---
+++ Makefile
@@ -0,0 +1,35 @@
+
+
+container:
+	# create/get docker container
+	docker build -f docker/Dockerfile -t pytorch-video-pipeline:latest .
+
+
+run-container: container
+	docker run -it --rm \
+		--privileged=true \
+		--ipc=host \
+		-v $(shell pwd):/app \
+		pytorch-video-pipeline:latest
+
+
+run-simple-cli: container
+	# run simple pipeline from CLI using gst-launch-1.0
+	docker run -it --rm \
+		--privileged=true \
+		--ipc=host \
+		-v $(shell pwd):/app \
+		pytorch-video-pipeline:latest \
+		gst-launch-1.0 filesrc location=media/in.mp4 ! decodebin ! progressreport update-freq=1 ! fakesink sync=true
+
+
+run-simple: container
+	# run simple.py in container
+	docker run -it --rm \
+		--privileged=true \
+		--ipc=host \
+		-v $(shell pwd):/app \
+		pytorch-video-pipeline:latest \
+		python simple.py
+
+
---
+++ simple.py
@@ -0,0 +1,37 @@
+
+import os, sys
+import gi
+gi.require_version('Gst', '1.0')
+
+from gi.repository import Gst
+
+Gst.init()
+
+pipeline = Gst.parse_launch('''
+    filesrc location=media/in.mp4 num-buffers=100 !
+    decodebin !
+    fakesink sync=true name=s
+''')
+
+
+def on_buffer(pad, info):
+    print(f'[{info.get_buffer().pts / Gst.SECOND:6.2f}]')
+    return Gst.PadProbeReturn.OK
+
+pipeline.get_by_name('s').get_static_pad('sink').add_probe(Gst.PadProbeType.BUFFER, on_buffer)
+
+
+pipeline.set_state(Gst.State.PLAYING)
+
+while True:
+    message = pipeline.get_bus().timed_pop_filtered(Gst.SECOND, Gst.MessageType.EOS | Gst.MessageType.ERROR)
+    if message is None: continue
+
+    structure = message.get_structure()
+    print('Message:', message.src.name, Gst.message_type_get_name(message.type), structure.to_string() if structure else '')
+
+    if message.type in (Gst.MessageType.EOS, Gst.MessageType.ERROR):
+        break
+
+Gst.debug_bin_to_dot_file(pipeline, Gst.DebugGraphDetails.ALL, 'FINALLY')
+pipeline.set_state(Gst.State.NULL)
